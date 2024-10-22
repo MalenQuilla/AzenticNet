a0fdbeccd48edf3e24951a98ff5b2ff17da7a0c4
malenquilla <dangviettung.works@gmail.com>
2024-10-22 10:05:14 +0700

    [develop][CDS] Fix: handle BadCredentialsException

--- common/src/main/java/malenquilla/cds/common/exception_handler/CommonExceptionHandler.java
+++ common/src/main/java/malenquilla/cds/common/exception_handler/CommonExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -75,6 +76,13 @@ public class CommonExceptionHandler {
                                     .toList());
 
         return this.createExceptionResponseEntity(HttpStatus.BAD_REQUEST, violationMessageSet);
+    }
+
+    @ExceptionHandler(value = BadCredentialsException.class)
+    private ResponseEntity<Object> handleBadCredentialsException(BadCredentialsException exception, WebRequest request) {
+        this. logException(request);
+
+        return this.createExceptionResponseEntity(HttpStatus.BAD_REQUEST, exception.getMessage());
     }
 
     @ExceptionHandler(value = RuntimeException.class)
